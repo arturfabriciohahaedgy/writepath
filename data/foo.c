e99a387a7c65f28ab1282ed3dfe99a86734c2ae4
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2022-10-09 14:08:41 -0300

    files used to test and demonstrate project

---
+++ data/foo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main()
+{
+    char *barpath = "";
+    char *bazpath;
+    char *bizpath;
+
+    bazpath = "";
+    bizpath = "sadasdasdsad";
+
+    return 0;
+}
